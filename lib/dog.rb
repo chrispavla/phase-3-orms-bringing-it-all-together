a419c4ab874178bb7ddd7e209a82a89893a5b1bd
Kristina <kristina.hodges7@gmail.com>
2022-09-03 12:39:47 -0400

    commit tree 398411747e7e3da53292f9c21cfd33737dbf9045

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
 
+  attr_accessor :id, :name, :breed 
+
+  def initialize (id: nil, name:, breed:)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+
+  def self.create_table
+    sql =  <<-SQL
+    DROP TABLE IF EXISTS dogs 
+    SQL
+    DB[:conn].execute(sql)
+  end
+
 end
